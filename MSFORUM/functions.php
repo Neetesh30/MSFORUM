64b7ae297ad5129ad9f30a9b38f6321ecca12267
Neetesh30 <34912189+Neetesh30@users.noreply.github.com>
2019-03-18 10:18:36 +0530

    update function.php

--- MSFORUM/functions.php
+++ MSFORUM/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+/*style link*/    
 function load_stylesheets(){
 	
 	wp_register_style('bootstrap',get_template_directory_uri().'/css/bootstrap.min.css',array(),false,'all');
@@ -26,8 +27,10 @@ function include_jquery(){
 }
 add_action('wp_enqueue_scripts','include_jquery');
 
+/*style link ends*/
 
 
+/*script link start*/
 function loadjs(){
 	
 	wp_register_script('customjs',get_template_directory_uri().'/js/scripts.js','',1,true);
@@ -38,7 +41,9 @@ function loadjs(){
 }
 add_action('wp_enqueue_scripts','loadjs');
 
+/*script link ends*/
 
+/*add menus link start*/
 add_theme_support('menus');
 
 
@@ -56,11 +61,15 @@ register_nav_menus(
         )
     
     );
+/*add menus link ends*/    
     
+    
+    /*image size*/    
     add_image_size('smallest',300,300,true);
     add_image_size('largest',800,800,true);
+    /*image size ends*/    
     
-    
+/*widget start*/    
 function ourWidgetsInit() {
     
     register_sidebar( array(
@@ -96,104 +105,10 @@ function ourWidgetsInit() {
     
 }    
     add_action('widgets_init','ourWidgetsInit');
+    /*widget ends*/    
     
-    
-    /* Custom Post Type Start */
-function create_posttype() {
-register_post_type( 'Products',
-// CPT Options
-array(
-  'labels' => array(
-   'name' => __( 'Products' ),
-   'singular_name' => __( 'Products' )
-  ),
-  'public' => true,
-  'has_archive' => false,
-  'rewrite' => array('slug' => 'Products'),
- )
-);
-}
-// Hooking up our function to theme setup
-add_action( 'init', 'create_posttype' );
-/* Custom Post Type End */
-
-
+  
 /*Custom Post type start*/
-function cw_post_type_products() {
-$supports = array(
-'title', // post title
-'editor', // post content
-'author', // post author
-'thumbnail', // featured images
-'excerpt', // post excerpt
-'custom-fields', // custom fields
-'comments', // post comments
-'revisions', // post revisions
-'post-formats', // post formats
-);
-$labels = array(
-'name' => _x('Products', 'plural'),
-'singular_name' => _x('Products', 'singular'),
-'menu_name' => _x('Products', 'admin menu'),
-'name_admin_bar' => _x('Products', 'admin bar'),
-'add_new' => _x('Add New', 'add new'),
-'add_new_item' => __('Add New Products'),
-'new_item' => __('New Products'),
-'edit_item' => __('Edit Products'),
-'view_item' => __('View Products'),
-'all_items' => __('All Products'),
-'search_items' => __('Search Products'),
-'not_found' => __('No Products found.'),
-);
-$args = array(
-'supports' => $supports,
-'labels' => $labels,
-'public' => true,
-'query_var' => true,
-'rewrite' => array('slug' => 'Products'),
-'has_archive' => true,
-'hierarchical' => false,
-);
-register_post_type('Products', $args);
-}
-
-register_taxonomy("Category", array("products"), array("hierarchical" => true, "label" => "Category", "singular_label" => "Category", "rewrite" => true));
-
-add_action('init', 'cw_post_type_products');
-
-
-function taxonomies_category() {
-    
-    $labels = array(
-'name' => _x('Products Category', 'plural'),
-'singular_name' => _x('Products Category', 'singular'),
-'menu_name' => __('Products Category', 'admin menu'),
-'add_new_item' => __('Add New Products Category'),
-'new_item_name' => __('New Products Category'),
-'edit_item' => __('Edit Products Category'),
-'update_item' => __('Update Products Category'),
-'view_item' => __('View Products Category'),
-'all_items' => __('All Products Category'),
-'search_items' => __('Search Products Category'),
-'parent_item' => __('Parent Products Category'),
-'parent_item_colon' => __('Parent Products Category'),
-'search_items' => __('Search Products Category'),
-);
-$args = array(
-'labels' => $labels,
-'heirarchial' => true,
-);    
-register_taxonomy('products_category', $args);    
-}
-add_action('init','taxonomies_category',0);
-/*Custom Post type end*/
-
-
-
-
-
-
-
 
 
 function awesome_custom_post_type (){
@@ -235,4 +150,5 @@ function awesome_custom_post_type (){
 	register_post_type('Product',$args);
 }
 add_action('init','awesome_custom_post_type');
+/*Custom Post type end*/
  ?>
